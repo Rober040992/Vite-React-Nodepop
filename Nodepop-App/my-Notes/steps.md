dbe51b2ddc61d7e320d4f1c9e35d42205da21db8
RoberGF <rgfrasta@gmail.com>
2025-02-08 19:05:01 +0100

    catching inputs form values

--- Nodepop-App/my-Notes/steps.md
+++ Nodepop-App/my-Notes/steps.md
@@ -2,3 +2,4 @@
 - creacion de sacffold con cliente
 - comienzo con el primer componente LoginPage
     1. creamos formulario con input para email y password y un boton de envio
+    2. estados de los inpunts con value y onchange
--- Nodepop-App/src/pages/auth/loginPage.tsx
+++ Nodepop-App/src/pages/auth/loginPage.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react'
+
 function LoginPage() {
+    const [email, setEmail] = useState('')
+    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value)
+    }
+
+    const [password, setPassword] = useState('')
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
     return (
         <div>
             <h3>Log In</h3>
             <form onSubmit={(event) => event.preventDefault()}>
                 <label htmlFor="email"> Email</label>
-                <input type="email" id="email" name="email" required />
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                    value={email}
+                    onChange={handleEmailChange}
+                />
                 <br />
-                <label htmlFor="passowrd"> Password</label>
-                <input type="password" id="password" name="password" required />
+                <label htmlFor="password"> Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
                 <br />
                 <button type="submit"> Submit </button>
             </form>
